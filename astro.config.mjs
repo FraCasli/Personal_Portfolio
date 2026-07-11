7aad5ea65ac47fe28d2d7a99911ad69160b8c34b
Your Name <gabriele.caslini@mail.polimi.it>
2026-07-11 22:59:22 +0200

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  site: "https://fracasli.github.io",
-  base: "/Personal_Portfolio",
+  site: "https://www.francescacaslini.info",
 });
